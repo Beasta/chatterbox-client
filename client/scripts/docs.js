27a351a28efcf61c92305390ef4e3cd4b52f1aa2
hdh3000 <henry.hutcheson@gmail.com>
2015-08-31 13:22:24 -0700

    Added a document outlining approach for sprint

---
+++ client/scripts/docs.js
@@ -0,0 +1,90 @@
+// YOUR CODE HERE:
+
+//  ___               
+// | . \ ___  ___  ___
+// |   // ._>/ . |<_-<
+// |_\_\\___.\_  |/__/
+//             |_|    
+
+
+
+//Get Bower up and running -- series of steps
+
+
+// Input messages
+// Display messages
+// Setup a way to refresh
+// Allow users to select userName/ send messages
+
+
+//allow users to create rooms
+//allow users to befriend other users
+//display text based on relation
+
+//complete backbone tutorial
+
+
+
+//  _ _ _            _    ___  _                 
+// | | | | ___  _ _ | |__| __>| | ___  _ _ _  ___
+// | | | |/ . \| '_>| / /| _> | |/ . \| | | |<_-<
+// |__/_/ \___/|_|  |_\_\|_|  |_|\___/|__/_/ /__/
+//                                               
+
+
+// work flow:
+// on page load: select user name
+// taken to family room
+// in any room  you some options
+	// can enter messages -- through dialog box
+	// can read messages -- displayed on screen/ auto updates
+	// change rooms -- dropdown box
+	// can friend -- clicking on user name
+		//friend persists across rooms
+
+
+
+
+
+ //      ___                         ___           ___         ___     
+ //     /\__\                       /\__\         /\  \       /\__\    
+ //    /:/ _/_         ___         /:/ _/_       /::\  \     /:/ _/_   
+ //   /:/ /\  \       /\__\       /:/ /\__\     /:/\:\__\   /:/ /\  \  
+ //  /:/ /::\  \     /:/  /      /:/ /:/ _/_   /:/ /:/  /  /:/ /::\  \ 
+ // /:/_/:/\:\__\   /:/__/      /:/_/:/ /\__\ /:/_/:/  /  /:/_/:/\:\__\
+ // \:\/:/ /:/  /  /::\  \      \:\/:/ /:/  / \:\/:/  /   \:\/:/ /:/  /
+ //  \::/ /:/  /  /:/\:\  \      \::/_/:/  /   \::/__/     \::/ /:/  / 
+ //   \/_/:/  /   \/__\:\  \      \:\/:/  /     \:\  \      \/_/:/  /  
+ //     /:/  /         \:\__\      \::/  /       \:\__\       /:/  /   
+ //     \/__/           \/__/       \/__/         \/__/       \/__/    
+
+// Get server rolling (hello world)
+	// pass a message
+	// pull down a message
+
+// protect from xss
+
+// revist architecture
+	//current thoguhts (room, message, user objects)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
